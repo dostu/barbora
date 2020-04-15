8327f7b12168b83a5976d47cbafa6a25f65eb00b
Donatas Stundys <donatas.stundys@gmail.com>
2020-04-15 19:42:22 +0300

    Specify how fast do you want your food

--- barbora.js
+++ barbora.js
@@ -1,21 +1,30 @@
-async function run() {
+const fetchDeliveries = async () => {
   const response = await fetch('https://www.barbora.lt/api/eshop/v1/cart/deliveries', {
     headers: {
       'Authorization': 'Basic YXBpa2V5OlNlY3JldEtleQ=='
     }
   })
-  const json = await response.json()
+  return response.json()
+}
 
-  const hours = json.deliveries[0].params.matrix.flatMap(day => day.hours)
-  const available = !!hours.find(hour => hour.available)
+const playAlarm = () => {
+  const audio = new Audio('https://sfxcontent.s3.amazonaws.com/soundfx/AircraftAlarm.mp3');
+  audio.loop = true
+  audio.play()
+}
 
-  console.log(`${new Date()} - ${available}`)
+const checkDeliveries = (daysInFuture) => async () => {
+  console.log(new Date())
 
-  if (available) {
-    const audio = new Audio('https://sfxcontent.s3.amazonaws.com/soundfx/AircraftAlarm.mp3');
-    audio.loop = true
-    audio.play()
+  const json = await fetchDeliveries()
+  const days = json.deliveries[0].params.matrix.slice(0, daysInFuture)
+  const hours = days.flatMap(day => day.hours)
+  const availableHours = hours.filter(hour => hour.available)
+
+  if (availableHours.length > 0) {
+    console.log(availableHours.map(hour => hour.deliveryTime))
+    playAlarm()
   }
 }
 
-setInterval(run, 60 * 1000)
+setInterval(checkDeliveries(2), 60 * 1000)
